1f2e01e86c2f684b3d61a6170db9831c982f15d2
Rokujima <officials.haruka@gmail.com>
2021-09-17 18:27:14 +0700

    update e2e User

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,44 +1,42 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-import { TaskModule } from './../src/task/task.module';
-import { UserModule } from './../src/user/user.module';
-import { createUserSuccess } from './mocks/user-sample-data/createUserSuccess.mocks';
 
 describe('UserController (e2e)', () => {
-  let app: INestApplication;
+  let app;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [UserModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = 'http://localhost:3000';
   });
 
-  it('/user (POST) ', (done) => {
-    return request(app.getHttpServer())
-      .post('/user')
-      .send(createUserSuccess) // x-www-form-urlencoded upload
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        if (err) return done(err);
-        return done();
-      });
-  });
-
-  it('/user (GET)', (done) => {
-    return request(app.getHttpServer())
+  it('/user (GET)', async () => {
+    return request(app)
       .get('/user')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200);
   });
 
+  it('/user (Update)', async () => {
+    return request(app)
+      .put('/user/6142037e68f41cc0397923c8')
+      .send({
+        email: 'testing-update-6142037e68f41cc0397923c8@gmail.com',
+        username: 'testing-username',
+      })
+      .expect(200);
+  });
 
-  afterAll(async () => {
-    await app.close();
+  it('/user (Delete)', async () => {
+    return request(app).delete('/user/6142037e68f41cc0397923c8').expect(200);
+  });
+
+  it('/user (POST)', () => {
+    return request(app)
+      .post('/user')
+      .set('Accept', 'application/json')
+      .send({
+        email: 'testing-update-6142037e68f41cc0397923c8@gmail.com',
+        username: 'testing-username',
+      })
+      .expect(201);
   });
 });
